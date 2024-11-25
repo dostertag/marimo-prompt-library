4054c63bc1669942557a52f09fa0149132acba71
Daniel Ostertag <daniel.ostertag@avispl.com>
2024-11-25 13:53:54 -0600

    initial personality traits prompt structure

--- prompt_library.py
+++ prompt_library.py
@@ -22,7 +22,7 @@ def __(prompt_library_module):
 def __(llm_module):
     llm_o1_mini, llm_o1_preview = llm_module.build_o1_series()
     llm_gpt_4o_latest, llm_gpt_4o_mini = llm_module.build_openai_latest_and_fastest()
-    # llm_sonnet = llm_module.build_sonnet_3_5()
+    llm_sonnet = llm_module.build_sonnet_3_5()
     # gemini_1_5_pro, gemini_1_5_flash = llm_module.build_gemini_duo()
 
     models = {
@@ -30,7 +30,7 @@ def __(llm_module):
         "o1-preview": llm_o1_preview,
         "gpt-4o-latest": llm_gpt_4o_latest,
         "gpt-4o-mini": llm_gpt_4o_mini,
-        # "sonnet-3.5": llm_sonnet,
+        "sonnet-3.5": llm_sonnet,
         # "gemini-1-5-pro": gemini_1_5_pro,
         # "gemini-1-5-flash": gemini_1_5_flash,
     }
